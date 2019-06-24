c9855ca4557cf8a460e1a04d1aec80e651a72af6
yuvraj <sunny@coderaft.com>
2019-06-24 17:20:00 +0530

    added task model for project

--- myproductivitytool/tasks/models.py
+++ myproductivitytool/tasks/models.py
@@ -4,6 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+PRIORITY = (
+    ('Low', 'Low'),
+    ('Medium', 'Medium'),
+    ('High', 'High'),
+)
+
 # Create your models here.
 class Project(models.Model):
     user = models.ForeignKey(User,default=None, related_name='project_user')
@@ -12,3 +18,13 @@ class Project(models.Model):
     class Meta:
         app_label = 'tasks'
 
+
+class Task(models.Model):
+    project = models.ForeignKey(Project,default=None, related_name='task_project')
+    task_name = models.CharField(max_length=500, default=None, blank=False)
+    prioriry = models.CharField(choices=PRIORITY,max_length=20, default='Disabled')
+    due_date = models.DateField(default=None, blank=True, null=True)
+    complete = models.BooleanField(default=False)
+
+    class Meta:
+        app_label = 'tasks'
